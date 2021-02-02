1ee0823853e894856d8557711eb35c0fd2dc1a83
Aji Saputro <ajisapuutro.s.kom@gmail.com>
2021-02-02 08:11:27 +0700

    Q1

---
+++ Q1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Q1
+//
+//  Created by Aji_sahputra on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var order: UIStackView!
+  @IBOutlet weak var addOrder: UIButton!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+     
+    
+  }
+
+  @IBAction func AddOrder(_ sender: Any) {
+  }
+}
+
